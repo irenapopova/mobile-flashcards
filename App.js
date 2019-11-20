bca2c5a7de598571eca645f3d59563c350f69f87
irenapopova <irenejpopova6@gmail.com>
2019-11-20 16:49:39 +0100

    add text

--- App.js
+++ App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View,Button, Alert } from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { View, StatusBar } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import { Feather } from "@expo/vector-icons";
+import { Constants } from "expo";
+import { white, purple, gray } from "./src/utils/colors";
+import { setLocalNotification } from "./src/utils/helpers";
 
+import reducer from "./src/reducers";
+import Decks from "./src/components/Decks";
+import AddDeck from "./src/components/AddDeck";
+import AddCard from "./src/components/AddCard";
+import DeckDetail from "./src/components/DeckDetail";
+import Quiz from "./src/components/Quiz";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HALLOOOO LITTLE RABBIT!</Text>
-      <Button
-      title="Press me"
-      onPress={() => Alert.alert('Simple Button pressed')}
-    />
+const FlashcardsStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
-    </View>
-  );
+const Tabs = createBottomTabNavigator(
+  {
+    Decks: {
+      screen: Decks,
+      navigationOptions: {
+        tabBarLabel: "Decks",
+        tabBarIcon: ({ tintColor }) => (
+          <Feather name="list" size={30} color={tintColor} />
+        )
+      }
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarLabel: "New Deck",
+        tabBarIcon: ({ tintColor }) => (
+          <Feather name="plus" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: purple,
+      style: {
+        height: 60,
+        backgroundColor: white,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      },
+      labelStyle: {
+        paddingTop: 3,
+        fontSize: 14,
+        fontWeight: "bold"
+      }
+    }
+  }
+);
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: Tabs,
+    DeckDetail: DeckDetail,
+    AddCard: AddCard,
+    Quiz: Quiz
+  },
+  {
+    initialRouteName: "Home",
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: { backgroundColor: purple },
+      headerTitleStyle: { fontWeight: "bold" }
+    }
+  }
+);
+
+class App extends Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <FlashcardsStatusBar
+            backgroundColor={purple}
+            barStyle="light-content"
+          />
+          <MainNavigator />
+        </View>
+      </Provider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
